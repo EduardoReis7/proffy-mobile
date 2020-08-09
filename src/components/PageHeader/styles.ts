61fe8f8143bb6e6575b244051c8f823647eb9b0c
EduardoReis7 <eduardodovigireis@gmail.com>
2020-08-08 23:28:44 -0300

    Conclusão do projeto web

--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -2,21 +2,31 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        padding: 40,
-        backgroundColor: '#8257E5'
+        backgroundColor: '#8257E5',
     },
     topBar: {
         flexDirection: 'row',
         alignItems: "center",
-        justifyContent: "space-between"
+        justifyContent: "space-between",
+        padding: 40,
     },
+
+    header: {   
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'space-between',
+        paddingRight: 40
+    },
+
     title: {
         fontFamily: 'Archivo_700Bold',
         color: '#FFF',
         fontSize: 24,
         lineHeight: 32,
         maxWidth: 160,
-        marginVertical: 40
+        marginVertical: 40,
+        marginLeft: 40,
+        marginTop: -15
     }
 });
 
